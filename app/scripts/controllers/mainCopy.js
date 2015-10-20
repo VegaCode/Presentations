b15be2e3835ab82c2ae55ad612c1711e9fb76343
= <cesarvega.col@gmail.com>
2015-10-20 12:51:18 -0400

    cimiting new code functionallyity

--- app/scripts/controllers/mainCopy.js
+++ app/scripts/controllers/mainCopy.js
@@ -57,7 +57,174 @@ angular.module('nwApp')
                                      };
                          };
 
-         
+            self.togglePresentation = function() {
+                        if (self.isTesNameTime === false) {
+                            self.isTesNameTime = true;
+                                   self.displaySettings = false;
+                        }else {
+                            self.isTesNameTime = false;
+                            self.displaySettings = true;
+                        }
+                    };
+
+            Reveal.addEventListener('overviewshown', function(event) {
+                        $rootScope.$apply(function() {
+                            self.isOverview = true;
+                        });
+                    });
+
+            Reveal.addEventListener('overviewhidden', function(event) {
+                        $rootScope.$apply(function() {
+                            self.isOverview = false;
+                        });
+                    });
+
+              //Warning! DO NOT CHANGE  THE "==" for "==="
+            Reveal.addEventListener('slidechanged', function(event) {
+                    // if (startTestNamesAtPage == event.indexh + 1) {
+                        if (self.presentTestNamesAtSlide == event.indexh + 1) {
+                            $rootScope.$apply(function() {
+                                self.isTesNameTime = false;
+                            });
+                        }
+                    });
+
+
+// **********  Getting Slides URL Images and the description for over view  *************************************************************************************
+    GetNamesAndSlides.getdata(projectId).then(function(result){
+                self.slides = result;
+                // slide show configuration settings
+                $timeout(function() {
+                    Reveal.initialize({
+
+
+                        width: 960,
+                        height: 680,
+
+                        // Factor of the display size that should remain empty around the content
+                        margin: 0.1,
+
+                        // Bounds for smallest/largest possible scale to apply to content
+                        minScale: 1.2,
+                        maxScale: 1.2,
+
+
+                        // Display controls in the bottom right corner
+                        controls: true,
+
+                        // Display a presentation progress bar
+                        progress: true,
+
+                        // Display the page number of the current slide
+                        slideNumber: true,
+
+                        // Push each slide change to the browser history
+                        history: false,
+
+                        // Enable keyboard shortcuts for navigation
+                        keyboard: true,
+
+                        // Enable the slide overview mode
+                        overview: true,
+
+                        // Vertical centering of slides
+                        center: true,
+
+                        // Enables touch navigation on devices with touch input
+                        touch: true,
+
+                        // Loop the presentation
+                        loop: false,
+
+                        // Change the presentation direction to be RTL
+                        rtl: false,
+
+                        // Turns fragments on and off globally
+                        fragments: true,
+
+                        // Flags if the presentation is running in an embedded mode,
+                        // i.e. contained within a limited portion of the screen
+                        embedded: false,
+
+                        // Flags if we should show a help overlay when the questionmark
+                        // key is pressed
+                        help: true,
+
+                        // Number of milliseconds between automatically proceeding to the
+                        // next slide, disabled when set to 0, this value can be overwritten
+                        // by using a data-autoslide attribute on your slides
+                        autoSlide: 0,
+
+                        // Stop auto-sliding after user input
+                        autoSlideStoppable: true,
+
+                        // Enable slide navigation via mouse wheel
+                        mouseWheel: true,
+
+                        // Hides the address bar on mobile devices
+                        hideAddressBar: false,
+
+                        // Opens links in an iframe preview overlay
+                        previewLinks: false,
+
+                        // Transition style
+                        transition: 'none', // none/fade/slide/convex/concave/zoom
+
+                        // Transition speed
+                        transitionSpeed: 'fast', // default/fast/slow
+
+                        // Transition style for full page slide backgrounds
+                        backgroundTransition: 'none', // none/fade/slide/convex/concave/zoom
+
+                        // Number of slides away from the current that are visible
+                        viewDistance: 1,
+
+                        // Parallax background image
+                        parallaxBackgroundImage: '', // e.g. "'https://s3.amazonaws.com/hakim-static/reveal-js/reveal-parallax-1.png'"
+
+                        // Parallax background size
+                        parallaxBackgroundSize: '', // CSS syntax, e.g. "2100px 900px"
+
+                        // Amount to move parallax background (horizontal and vertical) on slide change
+                        // Number, e.g. 100
+                        parallaxBackgroundHorizontal: '',
+                        parallaxBackgroundVertical: '',
+
+                        dependencies: [{
+                            src: 'bower_components/reveal-js/lib/js/classList.js',
+                            condition: function() {
+                                return !document.body.classList;
+                            }
+                        }, {
+                            src: 'bower_components/reveal-js/plugin/markdown/marked.js',
+                            condition: function() {
+                                return !!document.querySelector('[data-markdown]');
+                            }
+                        }, {
+                            src: 'bower_components/reveal-js/plugin/markdown/markdown.js',
+                            condition: function() {
+                                return !!document.querySelector('[data-markdown]');
+                            }
+                        }, {
+                            src: 'bower_components/reveal-js/plugin/highlight/highlight.js',
+                            async: true,
+                            callback: function() {
+                                hljs.initHighlightingOnLoad();
+                            }
+                        }, {
+                            src: 'bower_components/reveal-js/plugin/notes/notes.js',
+                            async: true,
+                            condition: function() {
+                                return !!document.body.classList;
+                            }
+                        }]
+                    });
+                    Reveal.configure({});
+                }, 300);
+            });
+
+
+
   // **********  Getting Slides TEST NAMES presentation  ****************************************************************************************************
 
         self.backGroundChanged = function(){
