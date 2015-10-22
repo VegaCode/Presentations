bc819e41e3d928e5f799721abf7ce67eb39ed0fe
unknown <cacevedo@CR-WK-CACEV.brandinstitute.com>
2015-10-22 14:18:06 -0400

    fixed another bug for dropdown that was not obtaining the current displayable background and also for the search bar that was not showing background

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -272,7 +272,14 @@ angular.module('nwApp')
                             _TestNameFontColor = slideObject[0].TestNameFontColor;_TestNameFontFamily = slideObject[0].TestNameFontFamily;
                             _ToNeutral =slideObject[0].TotNeutral; _ToPositive = slideObject[0].TotPositive;
 
-                            self.BackGround = slideObject[0].SlideBGFileName;
+                            if(slideObject[0].SlideBGFileName === ""){
+                              self.BackGround = slideObject[0].TemplateFileName;
+                            }else {
+                              self.BackGround = slideObject[0].SlideBGFileName;
+                            }
+
+                            self.BackGroundName = _TemplateName;
+
                             if(_SlideType === 'Image'){
                                  self.displayNameGroup = true;
                                  self.controlsPosition = -286;
