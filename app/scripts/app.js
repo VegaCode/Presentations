45d83b2e0d48a57948d7ac74b46151ed17352a65
unknown <cacevedo@CR-WK-CACEV.brandinstitute.com>
2015-10-27 08:49:32 -0400

    added tabs options for summary slide and fixed bugs

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,7 +19,7 @@ angular.module('nwApp', [
     'angularScreenfull',
     'AutoFontSize',
     'ui.bootstrap',
-     'cfp.hotkeys'
+    'cfp.hotkeys'
   ])
   .config(function ($routeProvider) {
     $routeProvider
