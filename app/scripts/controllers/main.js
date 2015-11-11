2808a0177c51f1e799256860b2f55b06a60fefa0
unknown <cacevedo@CR-WK-CACEV.brandinstitute.com>
2015-11-11 08:19:59 -0500

    fixed mustRank function bug. it no longer moves from slide to slide when popup shows

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -400,14 +400,14 @@ angular.module('nwApp')
         // CA- requires users to rank each testName
         self.mustRank = function(){
           if (self.nameRamking === "False" || self.nameRamking === false){
-            alertify.confirm("Please Rank the Name").set('onok', function(closeEvent){})
-            .set('oncancel', function(closeEvent){
-              alertify.alert("You are about to move to the Next Slide")
-              .set('onok', function(closeEvent){
-                var slideModel = JSON.stringify( new slideInfoModel(projectId, self.pageNumber, self.nameRamking, self.newName, self.explore,self.avoid, 'Next'));
-                getTestNamesObject(slideModel);
-              }).set('title', 'Moving to next slide');
-            }).set('title', 'Ranking Names');
+            var isNotRanked = confirm("Name has not been Ranked");
+            if (isNotRanked == true){
+              alert("Please rank the name before moving forward");
+            }else{
+              alert("Skipping Slide");
+              var slideModel = JSON.stringify( new slideInfoModel(projectId, self.pageNumber, self.nameRamking, self.newName, self.explore,self.avoid, 'Next'));
+              getTestNamesObject(slideModel);
+            }
           }else{
             var slideModel = JSON.stringify( new slideInfoModel(projectId, self.pageNumber, self.nameRamking, self.newName, self.explore,self.avoid, 'Next'));
             getTestNamesObject(slideModel);
