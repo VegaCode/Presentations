78a74ff6d69a4065b41ca2be562bf76b5c6d9698
= <cesarvega.col@gmail.com>
2015-11-30 09:25:04 -0500

    testing in progress

--- test/spec.js
+++ test/spec.js
@@ -3,14 +3,21 @@ describe('Protractor Demo App', function() {
   it('should have a title', function() {
     browser.get('http://localhost:9001/#/main/1012');
     browser.actions().sendKeys(protractor.Key.ARROW_UP).perform();
-    //protractor.Key.chord(protractor.Key.ARROW_UP)
+    
+  var el = element(by.id('navright'));
+  el.click();
+  el.click();
+  el.click();
+    
+    
+    
+
    element(by.model('main.selectedName')).sendKeys('LOXARESC');
     // element(by.model('second')).sendKeys(2);
     browser.actions().sendKeys(protractor.Key.ENTER).perform();
     browser.sleep(10000);
     // element(by.id('gobutton')).click();
-    expect(by.model('main.nameCandidate')).toEqual('LOXARESC');
+    expect(by.model('main.nameCandidate').toString()).toEqual('LOXARESC');
     expect(browser.getTitle()).toEqual('NW');
-    expect(browser.getTitle()).toEqual('N');
   });
 });
