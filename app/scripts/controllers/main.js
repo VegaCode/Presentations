ce093932ceddea1f84272aa19b2be05f8c4128ed
unknown <cacevedo@CR-WK-CACEV.brandinstitute.com>
2015-12-03 12:23:37 -0500

    added feature to search titles in typeahead, needs review

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -26,6 +26,7 @@ angular.module('nwApp')
             self.displayMenu = false;
             self.negativeKanaNames = '';
 
+            self.slidesNames = [];
             self.testName = [];
             self.isOverview = false;
 
@@ -156,6 +157,11 @@ self.greeting = 'Hello World!';
              $http.get(webBaseUrl + apicall + projectId).success(function(result){
                    _nameSummarySlideNumber = result[0].SummarySlide;
                    self.slides = result;
+
+                   result.map(function(obj){
+                     self.slidesNames.push(obj.SlideDescription);
+                   });
+
                    // slide show configuration settings
                    $timeout(function() {
                      Reveal.initialize({
@@ -809,6 +815,20 @@ self.greeting = 'Hello World!';
         self.selectedName = "";
 
         self.onSelect = function(slideName) {
+          var foundMatch = false;
+          var amount = 0;
+          if(self.testName.indexOf(slideName) < 0){
+            while(foundMatch == false){
+              if(slideName == self.slides[amount].SlideDescription){
+                var numberOfPage = self.slides[amount].$id;
+                self.selectSlide(parseInt(numberOfPage)-1);
+                foundMatch = true;
+              }else {
+                amount += 1;
+                foundMatch = false;
+              }
+            }
+          }else{
             self.selectedName = slideName;
             var query = projectId + ',' + "'" + slideName + "'";
             apiCall = 'api/NW_NamesAndSlides?projectIdAndTestName=';
@@ -821,6 +841,7 @@ self.greeting = 'Hello World!';
             } else {
                 self.progressBarValue = (parseInt(self.pageNumber) * self.progressBarUnit);
             }
+          }
         };
 
         self.logKey = function(event) {
@@ -829,7 +850,7 @@ self.greeting = 'Hello World!';
             }
         }
 
-        // getting the list of test names for the typeahead
+        // this list is to compare with displayable names for typeahead
         apiCall = 'api/NW_Presentation?projectIdForData=';
         $http.get(webBaseUrl + apiCall + projectId).success(function(testnames) {
             testnames.map(function(obj) {
