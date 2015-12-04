89884850237b5bce1f491ef616e6dbc30bc5162c
= <cesarvega.col@gmail.com>
2015-12-04 14:24:03 -0500

    commit to pull

--- app/scripts/directives/blur-me.js
+++ app/scripts/directives/blur-me.js
@@ -1,6 +1,16 @@
 
 // Blur directive is use via attribute <div blur-me /> and it will blur button or inputc boxes
-angular.module('nwApp').directive('blurMe', function blurElemDirective() {
+angular.module('nwApp')
+.directive('blurMe', function blurElemDirective() {
+  return {
+    restrict: 'A',
+    link: function (scope, element, attrs) {
+      element.bind('click', function () {
+        element.blur();
+      });
+    }
+  };
+}).directive('redMe', function blurElemDirective() {
   return {
     restrict: 'A',
     link: function (scope, element, attrs) {
