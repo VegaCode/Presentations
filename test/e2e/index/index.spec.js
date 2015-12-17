7a395645ffffce846ee39deab2a1dd246ef3783b
= <cesarvega.col@gmail.com>
2015-12-17 16:32:39 -0500

    commit to pull

--- test/e2e/index/index.spec.js
+++ test/e2e/index/index.spec.js
@@ -15,37 +15,35 @@ describe('Protractor Demo App', function() {
     expect(page.getTitle()).toEqual('NW');
   });
 
-  // it('should go through slides', function(){
-  //   page.continueMoving();
-  // });
-  //
-  // it('should open prompt for login password', function(){
-  //   page.switchToAlert('admin123');
-  //   page.sleep(8000);
-  // });
-  //
-  // it('should reset the whole project', function(){
-  //   page.sleep(3000);
-  //   page.clickButtonResetProject();
-  //   page.alertAccept();
-  // });
-  //
-  // it('should go home if reseting project does not go home', function(){
-  //   page.clickButtonGoHome();
-  //   page.alertAccept();
-  // });
+  it('should go through slides', function(){
+    page.continueMoving();
+  });
 
+  it('should open prompt for login password', function(){
+    page.switchToAlert('admin123');
+    page.sleep(8000);
+  });
 
+  it('should reset the whole project', function(){
+    page.sleep(3000);
+    page.clickButtonResetProject();
+    page.alertAccept();
+  });
+
+  it('should go home if reseting project does not go home', function(){
+    page.clickButtonGoHome();
+    page.alertAccept();
+  });
 
   it('should go through slides', function(){
     page.continueMoving();
   });
 
-  // it('should no longer display the settings for the admin by clicking the cog button', function(){
-  //   page.sleep(3000);
-  //   page.clickButtonAdminLogin();
-  //   page.sleep(2000);
-  // });
+  it('should no longer display the settings for the admin by clicking the cog button', function(){
+    page.sleep(3000);
+    page.clickButtonAdminLogin();
+    page.sleep(2000);
+  });
   it('should display notification of what we are doing in name testing', function(){
     page.clickButtonGrowlTrigger();
   });
@@ -59,65 +57,64 @@ describe('Protractor Demo App', function() {
     page.sleep(2000);
   });
 
-  // it('should show retained names', function(){
-  //   page.clickDisplayRetainedNames();
-  //   expect(page.retainedNamesAreDisplayed()).toBeTruthy();
-  //   page.sleep(2000);
-  // });
-  //
-  // it('should show Retained names', function(){
-  //   page.stackedProgressBarClick();
-  //   page.sleep(8000);
-  // });
-  //
-  // it('should show negative names', function(){
-  //   page.clickDisplayNegativeNames();
-  //   expect(page.negativeNamesAreDisplayed()).toBeTruthy();
-  //   page.sleep(2000);
-  // });
-  //
-  // it('should display new names', function(){
-  //   page.clickDisplayNewNames();
-  //   expect(page.newNamesAreDisplayed()).toBeTruthy();
-  //   page.sleep(2000);
-  // });
-  // it('should display roots to explore', function(){
-  //   page.clickDisplayExploreRoots();
-  //   expect(page.exploreRootsAreDisplayed()).toBeTruthy();
-  //   page.sleep(2000);
-  // });
-  // it('should display roots to avoid', function(){
-  //   page.clickDisplayAvoidRoots();
-  //   expect(page.avoidRootsAreDisplayed()).toBeTruthy();
-  //   page.sleep(2000);
-  // });
-  //
-  // it('should display information of the TypeAhead functionality', function(){
-  //   page.clickButtonGrowlTriggerTypeAhead();
-  //   page.sleep(2000);
-  // });
-  // it('should input a name in Search Bar and go to slide of the name', function(){
-  //   page.sendKeysOfSelectedName('APTIERA');
-  //   page.pressKeyEnter();
-  //   expect(page.getTextOfSelectedName()).toBe('APTIERA');
-  //   page.sleep(2000);
-  // });
-  //
-  // it('should reset the slide and then change the rank', function(){
-  //   page.clickButtonResetSlide();
-  //   page.clickRadioButtonPositive();
-  // });
-  //
-  // it('should display Tally', function(){
-  //   page.sleep(2000);
-  //   page.clickButtonTally();
-  //   expect(page.tallyIsDisplayed()).toBe(true);
-  // });
-  //
-  // it('should go to summary slide', function(){
-  //   page.clickButtonGoToSummary();
-  //   page.clickButtonGoToSummary();
-  //   page.sleep(2000);
-  // });
+  it('should show retained names', function(){
+    page.clickDisplayRetainedNames();
+    expect(page.retainedNamesAreDisplayed()).toBeTruthy();
+    page.sleep(2000);
+  });
+
+  it('should show Retained names', function(){
+    page.stackedProgressBarClick();
+    page.sleep(8000);
+  });
+
+  it('should show negative names', function(){
+    page.clickDisplayNegativeNames();
+    expect(page.negativeNamesAreDisplayed()).toBeTruthy();
+    page.sleep(2000);
+  });
+
+  it('should display new names', function(){
+    page.clickDisplayNewNames();
+    expect(page.newNamesAreDisplayed()).toBeTruthy();
+    page.sleep(2000);
+  });
+  it('should display roots to explore', function(){
+    page.clickDisplayExploreRoots();
+    expect(page.exploreRootsAreDisplayed()).toBeTruthy();
+    page.sleep(2000);
+  });
+  it('should display roots to avoid', function(){
+    page.clickDisplayAvoidRoots();
+    expect(page.avoidRootsAreDisplayed()).toBeTruthy();
+    page.sleep(2000);
+  });
+
+  it('should display information of the TypeAhead functionality', function(){
+    page.clickButtonGrowlTriggerTypeAhead();
+    page.sleep(2000);
+  });
+  it('should input a name in Search Bar and go to slide of the name', function(){
+    page.sendKeysOfSelectedName('APTIERA');
+    page.pressKeyEnter();
+    expect(page.getTextOfSelectedName()).toBe('APTIERA');
+    page.sleep(2000);
+  });
+
+  it('should reset the slide and then change the rank', function(){
+    page.clickButtonResetSlide();
+    page.clickRadioButtonPositive();
+  });
+
+  it('should display Tally', function(){
+    page.sleep(2000);
+    page.clickButtonTally();
+    expect(page.tallyIsDisplayed()).toBe(true);
+  });
+
+  it('should go to summary slide', function(){
+    page.clickButtonGoToSummary();
+    page.sleep(2000);
+  });
 
 });
