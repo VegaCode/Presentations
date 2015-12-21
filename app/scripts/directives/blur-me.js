d9d98550ca0605f51b3ffec6e7b0cc9b5f56388e
= <cesarvega.col@gmail.com>
2015-12-21 12:14:28 -0500

    katkana fixe with red-me directive

--- app/scripts/directives/blur-me.js
+++ app/scripts/directives/blur-me.js
@@ -16,13 +16,12 @@ angular.module('nwApp')
  return {
    restrict: 'A',
    link: function (scope, element) {
-     var color = element.css('color');
-      element.addClass('color');// for karma tests
      element.bind('click', function () {
+       var color = element.css('color');
        if(color === 'rgb(0, 0, 0)'){//test if color is black
-         element.css('color', 'red');
+         element.css('color', 'rgb(255, 0, 0)');
        }else{
-         element.css('color', 'black');
+         element.css('color', 'rgb(0, 0, 0)');
        }
      });
    }
