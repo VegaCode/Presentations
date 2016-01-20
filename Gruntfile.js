55d74afa5dad8cf2a935079c82682f1f8da2e08a
Cesar <cesarvega.col@gmail.com>
2016-01-20 09:34:08 -0500

    Revert "commitning patch to add new theme CenturyGothic"
    This reverts commit c5d91de82facd8aac0987397697045fdc173433d.

--- Gruntfile.js
+++ Gruntfile.js
@@ -408,6 +408,7 @@ module.exports = function (grunt) {
     }
   });
 
+
   grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {
     if (target === 'dist') {
       return grunt.task.run(['build', 'connect:dist:keepalive']);
