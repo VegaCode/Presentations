d953a55c5b0b46ded639c352f5814845e83c9b6b
cacevedo <cacevedo@brandinstitute.com>
2016-01-20 10:33:34 -0500

    commiting to pull

--- test/e2e/index/index.spec.js
+++ test/e2e/index/index.spec.js
@@ -7,7 +7,7 @@ describe('Protractor Demo App', function() {
   var page = new indexPage();
 
   jasmine.DEFAULT_TIMEOUT_INTERVAL =2000000;
-  page.AppUrl('https://tools.brandinstitute.com/nw_development/#/main/a2');
+  page.AppUrl('http://localhost:3000/#/main/625b');
 
   it("should show correct title and displays menu", function(){
     page.sendKeyBoard('ARROW_UP');
@@ -19,29 +19,29 @@ describe('Protractor Demo App', function() {
     page.continueMoving({id: 'navright'});
   });
 
-  it('should open prompt for login password', function(){
-    page.elementClick({id: 'cogSettings'});
-    page.AppSleep(1000);
-    page.switchToPopupString('admin123');
-    page.AppSleep(1000);
-    page.switchToPopup('accept');
-    page.AppSleep(1000);
-  });
-
-  it('should reset the whole project', function(){
-    page.elementClick({id: 'resetProject'});
-    page.switchToPopup('accept');
-    page.AppSleep(1000);
-  });
-
-  it('should go through slides', function(){
-    page.continueMoving({id: 'navright'});
-  });
-
-  it('should no longer display the settings for the admin by clicking the cog button', function(){
-    page.elementClick({id: 'cogSettings'});
-    page.AppSleep(1000);
-  });
+  // it('should open prompt for login password', function(){
+  //   page.elementClick({id: 'cogSettings'});
+  //   page.AppSleep(1000);
+  //   page.switchToPopupString('admin123');
+  //   page.AppSleep(1000);
+  //   page.switchToPopup('accept');
+  //   page.AppSleep(1000);
+  // });
+  //
+  // it('should reset the whole project', function(){
+  //   page.elementClick({id: 'resetProject'});
+  //   page.switchToPopup('accept');
+  //   page.AppSleep(1000);
+  // });
+  //
+  // it('should go through slides', function(){
+  //   page.continueMoving({id: 'navright'});
+  // });
+  //
+  // it('should no longer display the settings for the admin by clicking the cog button', function(){
+  //   page.elementClick({id: 'cogSettings'});
+  //   page.AppSleep(1000);
+  // });
 
   it('should start ranking Names and providing input information', function(){
     page.testNames();
@@ -78,9 +78,9 @@ describe('Protractor Demo App', function() {
 
   it('should input something in the comments for roots to explore', function(){
     page.sendStringToBox({id: 'exploreTextArea'}, 'Test Explore');
-    page.elementClick({id: 'saveExploreComments'});
-    page.switchToPopup('accept');
-    page.AppSleep(1000);
+    // page.elementClick({id: 'saveExploreComments'});
+    // page.switchToPopup('accept');
+    // page.AppSleep(1000);
   });
 
   it('should display roots to avoid', function(){
@@ -91,8 +91,8 @@ describe('Protractor Demo App', function() {
 
   it('should input something in the comments for roots to avoid', function(){
     page.sendStringToBox({id: 'avoidTextArea'}, 'Test Avoid');
-    page.elementClick({id: 'saveAvoidComments'});
-    page.switchToPopup('accept');
-    page.AppSleep(1000);
+    // page.elementClick({id: 'saveAvoidComments'});
+    // page.switchToPopup('accept');
+    // page.AppSleep(1000);
   });
 });
